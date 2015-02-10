425e147829cc7d1f850f8d757372e25a513729de
unknown <neild@DEVWS300.hq.trayport.com>
2015-02-10 08:11:28 +0000

    Improvement number 2 - decouple draw and update

--- AsyncGame/GameXna/Program.fs
+++ AsyncGame/GameXna/Program.fs
@@ -1,13 +1,16 @@
 ﻿open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 
+type LoopState = 
+| Update of GameTime
+| Draw of GameTime
+
 type XnaGame() as this =
     inherit Game()
 
     let initializeEvt = Event<_>()
     let loadEvt = Event<_>()
-    let updateEvt = Event<_>()
-    let drawEvt = Event<_>()
+    let loopEvt = Event<_>()
 
     do this.Content.RootDirectory <- "XnaGameContent"
     let graphicsDeviceManager = new GraphicsDeviceManager(this)
@@ -17,6 +20,7 @@ type XnaGame() as this =
         graphicsDeviceManager.PreferredBackBufferWidth <- 640
         graphicsDeviceManager.PreferredBackBufferHeight <- 480
         graphicsDeviceManager.ApplyChanges() 
+        game.IsFixedTimeStep <- false
         initializeEvt.Trigger()
         base.Initialize()
 
@@ -24,15 +28,14 @@ type XnaGame() as this =
         loadEvt.Trigger()
         
     override game.Update gameTime = 
-        updateEvt.Trigger gameTime
+        loopEvt.Trigger (Update gameTime)
 
     override game.Draw gameTime = 
-        drawEvt.Trigger gameTime
+        loopEvt.Trigger (Draw gameTime)
         
     member game.InitializeAsync =  Async.AwaitEvent initializeEvt.Publish
     member game.LoadContentAsync =  Async.AwaitEvent loadEvt.Publish
-    member game.UpdateAsync =  Async.AwaitEvent updateEvt.Publish
-    member game.DrawAsync =  Async.AwaitEvent drawEvt.Publish
+    member game.LoopAsync =  Async.AwaitEvent loopEvt.Publish
 
 let game = new XnaGame()
 
@@ -43,20 +46,20 @@ let gameWorkflow = async {
     let sprite = game.Content.Load<Texture2D>("Sprite")
 
     let rec gameLoop x y dx dy = async {
-        let! time = game.UpdateAsync
-        let dx = if x > 608.f || x < 0.f then -dx else dx
-        let dy = if y > 448.f || y < 0.f then -dy else dy
-        
-        let x = x + dx
-        let y = y + dy
-
-        let! time = game.DrawAsync
-        game.GraphicsDevice.Clear(Color.CornflowerBlue)
-        spriteBatch.Begin()
-        spriteBatch.Draw(sprite, Vector2(x,y), Color.White)
-        spriteBatch.End()
-
-        return! gameLoop x y dx dy
+        let! nextState = game.LoopAsync
+        match nextState with
+        | Update time -> 
+            let dx = if x > 608.f || x < 0.f then -dx else dx
+            let dy = if y > 448.f || y < 0.f then -dy else dy
+            let x = x + dx
+            let y = y + dy
+            return! gameLoop x y dx dy
+        | Draw time -> 
+            game.GraphicsDevice.Clear(Color.CornflowerBlue)
+            spriteBatch.Begin()
+            spriteBatch.Draw(sprite, Vector2(x,y), Color.White)
+            spriteBatch.End()
+            return! gameLoop x y dx dy
     }
     return! gameLoop 0.f 0.f 4.f 4.f
 }
